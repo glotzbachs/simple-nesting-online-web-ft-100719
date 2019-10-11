cf8e20c2827396d4741df20eb1e2f6339b2279dc
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 05:24:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,36 +60,37 @@ def dennis_ritchies_language
      programmer_hash[:dennis_ritchie][:languages][0]
 end
 
-# def adding_matz
+def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
 
-# 	programmer_hash = 
-# 		{
-#         :grace_hopper => {
-#           :known_for => "COBOL",
-#           :languages => ["COBOL", "FORTRAN"]
-#         },
-#         :alan_kay => {
-#           :known_for => "Object Orientation",
-#           :languages => ["Smalltalk", "LISP"]
-#         },
-#         :dennis_ritchie => {
-#           :known_for => "Unix",
-#           :languages => ["C"]
-#         }
-#     }
+	programmer_hash = 
+		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+    }
 
-#     programmer_hash.unshift={
-#       :yukihiro_matsumoto => {
-#         :known_for=>"Ruby", 
-#         :languages => ["LISP", "C"]
-#       }
-#     }
-# end
+    programmer_hash.unshift={
+      :yukihiro_matsumoto => {
+        :known_for=>"Ruby", 
+        :languages => ["LISP", "C"]
+      }
+    }
+    programmer_hash
+end
 
 def changing_alan
 	programmer_hash = 
@@ -110,7 +111,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for]=alans_new_info
-     
+     programmer_hash
 end
 
 def adding_to_dennis
